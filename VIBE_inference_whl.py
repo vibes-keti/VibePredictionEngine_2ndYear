6a6eb9f707c4eeace8ddd863a03256567ca5f0f9
jaewonMoon <jaewonmoon.keti@gmail.com>
2021-08-19 17:10:31 +0900

    min

--- VIBE_inference_whl.py
+++ VIBE_inference_whl.py
@@ -13,7 +13,7 @@ def inference(data_set, VI):
     test = DFS.scaling_dataset(data_set)
     
     learning_information = VI.learning_information
-    data_frequency_sec = pd.to_timedelta(to_offset(pd.infer_freq(data_set[:5].index)), errors='coerce').total_seconds()
+    #data_frequency_sec = pd.to_timedelta(to_offset(pd.infer_freq(data_set[:5].index)), errors='coerce').total_seconds()
     TestDataSet = LDS.LearningDataSet(learning_information)
     test_X,learning_information= TestDataSet.get_inference_X(test)
     LSTM_P = LSTM_m.Predict(VI.model_file_name)
@@ -26,24 +26,27 @@ def inference(data_set, VI):
 
 
 if __name__ == "__main__":
-    import sys
-    sys.path.append("../")
-    sys.path.append("../..")
+
+    
+    ## 0.예측에 필요한 적절한 모델과 전처리를 선택하기 위한 파라미터 셋팅
+    # test_parameters에 주요 파라미터를 기술해놓음
+    # ㅖㅁ
+    # 현재는 80분 동안의 데이터를 바탕으로 12분 이후의 값을 예측함 
+    from settings import vibe_setting as vls
+    from settings import test_parameters as ts
+    VI= vls.vibe_learning(ts.features, ts.time_min, ts.learning_method_num)
+    #########################
+
+    ## 1. 예측에 필요한 입력 데이터
 
     import os
     dirname = os.path.dirname(__file__)
     test_data_fileName = os.path.join(dirname, 'data','test_data.csv')
-    print(test_data_fileName)
-    
-    from settings import vibe_setting as vls
-    from settings import test_parameters as ts
-
-    VI= vls.vibe_learning(ts.features, ts.time_min, ts.learning_method_num)
-
-     ## dataset preparation ##
     data_set = pd.read_csv(test_data_fileName, index_col=['datetime'], parse_dates=['datetime']) 
+
     data_set = data_set[VI.feature_list]
     data_set = data_set[-VI.X_row_num:]
-    #########################
+    ######################################
+
     inv_yhat= inference(data_set, VI)  
     print(inv_yhat)
